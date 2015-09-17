9cc6de0f63af46e20d5650d8afa3e2dc4fc50ae4
haifeng.yan@hpe.com <yanhaif@c9t13559.itcs.hpecorp.net>
2015-09-17 08:34:27 +0000

    917 update

--- grid.py
+++ grid.py
@@ -1,4 +1,4 @@
 import sys
 if __name__ == '__main__':
     price = float(sys.argv[1])
-    print [round(i*1000)/1000.0 for i in [price * 0.92, price * 0.96, price * 1.04, price * 1.08]]
+    print [round(i*1000)/1000.0 for i in [price * 0.94, price * 0.98, price * 1.02, price * 1.06]]
