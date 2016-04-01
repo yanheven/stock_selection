98ddfb518f455f09b841fd0d07e2107d44d4aa3b
yan.haifeng <yanheven@qq.com>
2016-04-01 17:46:47 +0800

    add post status

--- today_trade.py
+++ today_trade.py
@@ -1,13 +1,20 @@
 #_*_ coding: UTF-8 _*_
 __author__ = 'evan'
 import time
+import json
+import requests
 
 from download import download_300_500
 from download import get_current_300_500
+from login import get_xueqiu_session
+from nomore_xueqiu import HEADERS
+import logger
 
+
+LOG = logger.get_loger()
 
 def predict():
-    history_data = download_300_500()
+    history_data = download_300_500(False)
     current_point = get_current_300_500()
     early_300 = history_data[0][0] / history_data[0][19] * 100 - 100
     early_500 = history_data[1][0] / history_data[1][19] * 100 - 100
@@ -24,9 +31,27 @@ def predict():
     print(change_300, change_500)
 
     current_time = time.strftime('%F %T',time.localtime())
-    message = current_time + ''' $蛋卷斗牛二八轮动(CSI001)$ 沪深300 中证500 与20天前对比涨幅分别为： '''\
+    message = current_time + ''' $蛋卷斗牛二八轮动(CSI001)$ $沪深300(SZ399300)$ $中证500(SH000905)$ 与20天前对比涨幅分别为： '''\
               + current_300 + '% , ' + current_500 + '%'
+    LOG.warn(message)
     print(message)
+    sess = get_xueqiu_session()
+    timestamp = str(time.time()*1000)
+    # token = sess.get('http://xueqiu.com/c/pin/session', headers=HEADERS)
+    token = sess.get('http://xueqiu.com/service/csrf?api=%2Fstatuses%2Fupdate.json&_=', headers=HEADERS)
+    LOG.warn(token)
+    token = json.loads(token.content)
+    token = str(token.get('token'))
+    LOG.warn(token)
+    body = {'status': '<p>' + message + '</p>',
+            'session_token': token}
+    url = 'http://xueqiu.com/statuses/update.json'
+    HEADERS.update({'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                    'Referer': 'http://xueqiu.com/afio',
+                    'X-Requested-With': 'XMLHttpRequest'})
+    ret = sess.post(url, data=body, headers=HEADERS)
+    print(ret)
+    LOG.warn(ret)
 
 
 if __name__ == '__main__':
