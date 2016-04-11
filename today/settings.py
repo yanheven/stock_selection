80a0bdeed1c2dc087260cd702557ad7d5fa99da9
yan.haifeng <yanheven@qq.com>
2016-04-11 16:10:27 +0800

    update debug

--- today/settings.py
+++ today/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd78nit565-ttcj9^lmv^2j(s=$i4undx6iwjsal!!fi2%d$$-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
