c27cb1ddfe6f43f2e4c4e3cfaf62c25135d8ac76
yan.haifeng <yanheven@qq.com>
2016-04-20 17:14:59 +0800

    add safari

--- safari.py
+++ safari.py
@@ -1,1 +1,35 @@
 __author__ = 'evan'
+import requests
+
+
+pre_url = 'https://www.safaribooksonline.com/library/view/openstack-in-action/9781617292163/'
+mid_url = 'fig'
+alt_url = '_alt'
+end_rul = '.jpg'
+chapters = {'a0': 41,
+            '01': 8,
+            '02': 19,
+            '03': 16,
+            '04': 18,
+            '05': 7,
+            '06': 14,
+            '07': 7,
+            '08': 4,
+            '9': 6,
+            '10': 2,
+            '11': 18,
+            '12': 10}
+for ch, total in chapters.items():
+    for i in xrange(1, int(total) + 1):
+        index = str(i)
+        if i < 10:
+            index = '0' + str(i)
+        preurl = pre_url + ch + mid_url + index
+        url1 = pre_url + end_rul
+        resp = requests.get(url1)
+        if resp.status_code != 200:
+            url2 = preurl + alt_url + end_rul
+            resp = requests.get(url2)
+        name = ch + mid_url + index + end_rul
+        with open(name, 'w') as fb:
+            fb.write(resp.content)
